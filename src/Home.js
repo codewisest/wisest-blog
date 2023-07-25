013690af7956129f20af0d2499cf56d73517bd0f
Chijioke <wisestcodes@gmail.com>
2023-07-25 11:53:04 -0400

    hook elements

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
+
 const Home = () => {
-  const handleClick = () => {
-    console.log("Hello ninjas");
+  // let name = "Wisest";
+  const [name, setName] = useState("Wisest");
+  const [age, setAge] = useState(30);
+
+  const updatePerson = () => {
+    setName("Chiji");
+    setAge(36);
   };
 
   const handleClickAgain = (name) => {
@@ -9,7 +16,10 @@ const Home = () => {
   return (
     <div className="home">
       <h2>Blog home page</h2>
-      <button onClick={handleClick}>Click me</button>
+      <p>
+        {name} is {age} years old
+      </p>
+      <button onClick={updatePerson}>Update person</button>
       <button
         onClick={() => {
           handleClickAgain("Chiji");
